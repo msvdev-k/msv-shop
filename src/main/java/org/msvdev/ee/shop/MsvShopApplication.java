cfefb6e45cb40bb552c1b433fdea3cf5501dbadd
msvdev-k <msvdev.k@gmail.com>
2022-11-26 12:45:38 +0300

    Init project

---
+++ src/main/java/org/msvdev/ee/shop/MsvShopApplication.java
@@ -0,0 +1,13 @@
+package org.msvdev.ee.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/msvdev/ee/shop/MsvShopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.msvdev.ee.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
